fb49a416aa1ae7e91462b01bdb4fd4135faf72e2
tsuzuki <goodin557@gmail.com>
2023-08-19 00:00:57 +0900

    複数ファイル対応

--- main.go
+++ main.go
@@ -6,30 +6,27 @@ import (
 	"image/jpeg"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func compressImage(inputPath, outputPath string, quality int) error {
-	// 画像ファイルを開く
 	file, err := os.Open(inputPath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// 画像をデコードする
 	img, _, err := image.Decode(file)
 	if err != nil {
 		return err
 	}
 
-	// 出力先のファイルを作成する
 	outFile, err := os.Create(outputPath)
 	if err != nil {
 		return err
 	}
 	defer outFile.Close()
 
-	// 画像をJPEG形式でエンコードして、品質を設定して保存する
 	opts := &jpeg.Options{Quality: quality}
 	err = jpeg.Encode(outFile, img, opts)
 	if err != nil {
@@ -40,26 +37,39 @@ func compressImage(inputPath, outputPath string, quality int) error {
 }
 
 func main() {
-	// 実行ファイルの絶対パスを取得
 	exePath, err := os.Executable()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	// 実行ファイルのディレクトリを取得
 	dir := filepath.Dir(exePath)
+	imgDir := filepath.Join(dir, "img")
 
-	// 画像へのパスを組み立てる
-	inputPath := filepath.Join(dir, "img", "input.jpg")
-	outputPath := filepath.Join(dir, "img", "output.jpg")
+	quality := 75
 
-	quality := 75 // 0から100までの品質。75は一般的な圧縮率です。
-
-	err = compressImage(inputPath, outputPath, quality)
+	// 指定ディレクトリ内のすべてのファイルとディレクトリをリストアップ
+	files, err := os.ReadDir(imgDir)
 	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Compression successful!")
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for _, f := range files {
+		filename := f.Name()
+
+		// .jpgファイルのみを対象とする
+		if strings.HasSuffix(strings.ToLower(filename), ".jpg") {
+			inputPath := filepath.Join(imgDir, filename)
+			// 例えば、input.jpgの場合、出力はoutput-input.jpgとする
+			outputPath := filepath.Join(imgDir, "output-"+filename)
+
+			err := compressImage(inputPath, outputPath, quality)
+			if err != nil {
+				fmt.Printf("Error compressing %s: %v\n", filename, err)
+			} else {
+				fmt.Printf("Compression of %s successful!\n", filename)
+			}
+		}
 	}
 }
